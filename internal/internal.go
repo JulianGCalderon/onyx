5d53c2dfe7e890b082fd511989996209fa67c0c8
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-04-21 00:07:46 -0300

    Add BuildDstPath utility

--- internal/internal.go
+++ internal/internal.go
@@ -1,1 +1,14 @@
 package internal
+
+import (
+	"juliangcalderon/onyx/internal/utils"
+	"path/filepath"
+)
+
+func BuildDstPath(path, root string) string {
+	if utils.IsMarkdown(path) {
+		path = utils.SetExt(path, ".html")
+	}
+
+	return filepath.Join(root, path)
+}
