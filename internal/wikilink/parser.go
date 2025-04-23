8e976b21c65fcbf59a914d493c802fd96cb3a8ef
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-04-22 23:36:21 -0300

    Use splitting instead of regex

--- internal/wikilink/parser.go
+++ internal/wikilink/parser.go
@@ -1,8 +1,7 @@
 package wikilink
 
 import (
-	"fmt"
-	"regexp"
+	"bytes"
 
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/parser"
@@ -13,57 +12,79 @@ type wikilinkParser struct {
 	resolver resolver
 }
 
+var (
+	tokenEmbed    []byte = []byte("![[")
+	tokenLink     []byte = []byte("[[")
+	tokenClose    []byte = []byte("]]")
+	tokenFragment []byte = []byte("#")
+	tokenTitle    []byte = []byte("|")
+)
+
 func (w *wikilinkParser) Trigger() []byte {
 	return []byte{'[', '!'}
 }
 
 func (p *wikilinkParser) Parse(parent ast.Node, block text.Reader, pc parser.Context) ast.Node {
-	reTarget := `([^|#]*?)`
-	reHash := `(#[^|]+?)?`
-	reTitle := `(\|.+?)?`
-	reFull := fmt.Sprintf(`^!?\[\[%v%v%v\]\]`, reTarget, reHash, reTitle)
+	source, _ := block.PeekLine()
 
-	_, segment := block.Position()
-	re := regexp.MustCompile(reFull)
-	matches := block.FindSubMatch(re)
-	if len(matches) != 4 {
+	var (
+		indexStart int
+		indexClose int
+		embed      bool
+	)
+
+	// Find start of wikilink
+	switch {
+	case bytes.HasPrefix(source, tokenEmbed):
+		embed = true
+		indexStart = len(tokenEmbed)
+	case bytes.HasPrefix(source, tokenLink):
+		embed = false
+		indexStart = len(tokenLink)
+	default:
 		return nil
 	}
-	target := matches[1]
-	hash := matches[2]
-	title := matches[3]
 
-	if len(target) == 0 && len(hash) <= 1 && len(title) <= 1 {
+	// Find end of wikilink
+	indexClose = bytes.Index(source, tokenClose)
+	if indexClose <= 0 {
 		return nil
 	}
 
-	embed := matches[0][0] == '!'
-	if embed {
-		segment.Start += len("!")
+	// Obtain full wikilink content
+	content := source[indexStart:indexClose]
+	if len(content) == 0 {
+		// Content cannot be empty
+		return nil
 	}
 
-	if len(title) == 0 {
-		segment.Start += len("[[")
-		title = append(target, hash...)
-	} else {
-		segment.Start += len("[[") + len(target) + len(hash) + len("|")
-		title = title[1:]
+	// Separate destination from title: [[destination|title]]
+	destination, title, cut := bytes.Cut(content, tokenTitle)
+	if cut && len(title) == 0 {
+		// Title cannot be empty
+		return nil
+	}
+	if !cut {
+		// If there is no specific title, fall back to the full destination
+		title = destination
 	}
 
-	if len(hash) > 0 {
-		hash = hash[1:]
+	// Separate target from fragment: [[target#fragment]]
+	target, fragment, cut := bytes.Cut(destination, tokenFragment)
+	if cut && len(fragment) == 0 {
+		// Fragment cannot be empty
+		return nil
 	}
 
-	segment.Stop = segment.Start + len(title)
-
-	hash = []byte(normalizeFragment(string(hash)))
-
-	destination := []byte(p.resolver.resolve(string(target), string(hash)))
+	// Resolve destination using the resolver
+	destination = []byte(p.resolver.resolve(string(target), string(fragment)))
 
 	link := ast.NewLink()
 	link.Title = title
 	link.Destination = destination
-	link.AppendChild(link, ast.NewTextSegment(segment))
+	link.AppendChild(link, ast.NewString(title))
+
+	block.Advance(indexClose + len(tokenClose))
 
 	if embed {
 		return ast.NewImage(link)
