d5d7f2905f65c4191790caa121b615d97555dfc9
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-05-04 19:44:19 -0300

    List content files in index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import { glob } from "glob";
+import process from "process";
+
+console.log("CWD:", process.cwd());
+
+const files = await glob("content/**");
+console.log("FILES:", files);
